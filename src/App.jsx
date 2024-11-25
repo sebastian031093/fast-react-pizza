2411032adfc46502202ec102af100d5f2acfba5a
Sebastian Manco G <sebasmg1393@gmail.com>
2024-11-25 18:51:47 -0500

    seba: error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./feactures/menu/Menu";
 import Cart from "./feactures/cart/Cart";
 import CreateOrder from "./feactures/order/CreateOrder";
 import Order from "./feactures/order/Order";
-import AppLayout from "./ui/appLayout";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 //TODO: Router better, or not better but whit objects
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -20,6 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
 
       {
